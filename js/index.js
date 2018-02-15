c1fa09da115d876c03d0d9b86ab864205ca57dba
mfrashad <m.fathyrashad@gmail.com>
2018-02-15 14:13:47 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,112 @@
+var quoteArr1 = [
+  {"quoteText":"Joy is the best makeup. ","quoteAuthor":"Anne Lamott","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/c8a0eeb816/"},{"quoteText":"Never regret. If it's good, it's wonderful. If it's bad, its experience.","quoteAuthor":"Victoria Holt","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/338891b01b/"},{"quoteText":"Peace of mind is not the absence of conflict from life, but the ability to cope with it.","quoteAuthor":"","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/30b2651987/"},{"quoteText":"Life is just a chance to grow a soul. ","quoteAuthor":"A. Powell Davies","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/fcce1bf481/"},{"quoteText":"The path to success is to take massive, determined action.  ","quoteAuthor":"Tony Robbins ","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/0813751abe/"},{"quoteText":"A dream is your creative vision for your life in the future. You must break out of your current comfort zone and become comfortable with the unfamiliar and the unknown.  ","quoteAuthor":"Denis Waitley","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/07734e2a6c/"},{"quoteText":"Only when we are no longer afraid do we begin to live. ","quoteAuthor":"Dorothy Thompson","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/34d545bd70/"},{"quoteText":"Everything that irritates us about others can lead us to an understanding about ourselves. ","quoteAuthor":"Carl Jung","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/80237530cf/"},{"quoteText":"Chance is always powerful. Let your hook be always cast; in the pool where you least expect it, there will be a fish. ","quoteAuthor":"Ovid","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/cdde4a8f41/"},{"quoteText":"It is better to have enough ideas for some of them to be wrong, than to be always right by having no ideas at all. ","quoteAuthor":"Edward de Bono","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/c4dc4bc865/"},{"quoteText":"Growth itself contains the germ of happiness. ","quoteAuthor":"Pearl Buck","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/a6db891f90/"},{"quoteText":"Growth itself contains the germ of happiness. ","quoteAuthor":"Pearl Buck","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/a6db891f90/"},{"quoteText":"To avoid criticism, do nothing, say nothing, be nothing. ","quoteAuthor":"Elbert Hubbard","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/19363e0b63/"},{"quoteText":"We must be as courteous to a man as we are to a picture, which we are willing to give the advantage of a good light. ","quoteAuthor":"Ralph Emerson","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/9acf4688ce/"},{"quoteText":"If A is success in life, then A equals x plus y plus z. Work is x; y is play; and z is keeping your mouth shut. ","quoteAuthor":"Albert Einstein ","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/198af145ad/"},{"quoteText":"The foot feels the foot when it feels the ground.  ","quoteAuthor":"Buddha ","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/7ce9030adf/"},{"quoteText":"Those who try to do something and fail are infinitely better than those who try nothing and succeed. ","quoteAuthor":"Lloyd Jones","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/1e49b16aee/"},{"quoteText":"If you want to study yourself — look into the hearts of other people. If you want to study other people — look into your own heart. ","quoteAuthor":"Friedrich von Schiller","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/b54662a106/"},{"quoteText":"Human beings, who are almost unique in having the ability to learn from the experience of others, are also remarkable for their apparent disinclination to do so.","quoteAuthor":"Douglas Adams","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/0bdc846b2e/"},{"quoteText":"I would rather be attacked than unnoticed. For the worst thing you can do to an author is to be silent as to his works. An assault upon a town is a bad thing; but starving it is still worse. ","quoteAuthor":"Samuel Johnson","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/ecde0e429d/"},{"quoteText":"To be what we are, and to become what we are capable of becoming, is the only end of life.  ","quoteAuthor":"Robert Stevenson","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/ab5cc88a65/"},{"quoteText":"With the realization of ones own potential and self-confidence in ones ability, one can build a better world.","quoteAuthor":"Dalai Lama","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/359ad6d20a/"},{"quoteText":"Failure will never overtake me if my determination to succeed is strong enough.  ","quoteAuthor":"Og Mandino","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/70b5a19641/"},{"quoteText":"Nothing strengthens authority so much as silence.  ","quoteAuthor":"Leonardo da Vinci","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/cb840d0ce0/"},{"quoteText":"Don't turn away from possible futures before you're certain you don't have anything to learn from them.","quoteAuthor":"Richard Bach","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/bc8ee1f756/"},{"quoteText":"Appreciation is the highest form of prayer, for it acknowledges the presence of good wherever you shine the light of your thankful thoughts. ","quoteAuthor":"Alan Cohen ","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/b1c20defbb/"},{"quoteText":"Three things cannot be long hidden: the sun, the moon, and the truth.  ","quoteAuthor":"Buddha ","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/7dc3d90ceb/"},{"quoteText":"Most folks are as happy as they make up their minds to be.","quoteAuthor":"Abraham Lincoln","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/8f581c933c/"},{"quoteText":"The right way is not always the popular and easy way. Standing for right when it is unpopular is a true test of moral character. ","quoteAuthor":"Margaret Smith","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/dbca3336fa/"},{"quoteText":"By living deeply in the present moment we can understand the past better and we can prepare for a better future.","quoteAuthor":"Thich Nhat Hanh","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/2c50be96f5/"},{"quoteText":"A house divided against itself cannot stand.","quoteAuthor":"Abraham Lincoln","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/25d6485021/"},{"quoteText":"Love is not blind; it simply enables one to see things others fail to see.","quoteAuthor":"","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/848e15db47/"},{"quoteText":"Your work is to discover your world and then with all your heart give yourself to it.  ","quoteAuthor":"Buddha","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/00f5f117c9/"},{"quoteText":"When we see men of worth, we should think of equaling them; when we see men of a contrary character, we should turn inwards and examine ourselves. ","quoteAuthor":"Confucius","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/75c9b9e602/"},{"quoteText":"The future is an opaque mirror. Anyone who tries to look into it sees nothing but the dim outlines of an old and worried face.  ","quoteAuthor":"Jim Bishop ","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/bc892c7baa/"},{"quoteText":"Genuine sincerity opens people's hearts, while manipulation causes them to close.","quoteAuthor":"Daisaku Ikeda","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/d4902a485a/"},{"quoteText":"Fine words and an insinuating appearance are seldom associated with true virtue ","quoteAuthor":"Confucius","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/ec0049c629/"},{"quoteText":"If we look at the world with a love of life, the world will reveal its beauty to us.","quoteAuthor":"Daisaku Ikeda","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/2bda825374/"},{"quoteText":"Trust your own instinct. Your mistakes might as well be your own, instead of someone elses. ","quoteAuthor":"Billy Wilder","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/fb54c230b3/"},{"quoteText":"Lots of people want to ride with you in the limo, but what you want is someone who will take the bus with you when the limo breaks down. ","quoteAuthor":"Oprah Winfrey","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/0cf8186c73/"},{"quoteText":"To will is to select a goal, determine a course of action that will bring one to that goal, and then hold to that action till the goal is reached. The key is action. ","quoteAuthor":"Michael Hanson","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/959e1d322c/"},{"quoteText":"Better than a thousand hollow words, is one word that brings peace.  ","quoteAuthor":"Buddha","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/0d8ff28195/"},{"quoteText":"Character cannot be developed in ease and quiet. Only through experience of trial and suffering can the soul be strengthened, vision cleared, ambition inspired, and success achieved. ","quoteAuthor":"Helen Keller","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/f650c84d41/"},{"quoteText":"I will not be concerned at other men is not knowing me;I will be concerned at my own want of ability.","quoteAuthor":"Confucius","senderName":"","senderLink":"","quoteLink":"http://forismatic.com/en/9e9eef8ce9/"}];
+var quoteArr = [];
+var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
+
+
+var quoteIndex = 0;
+var nextBtn = document.getElementById("btn-right");
+var prevBtn = document.getElementById("btn-left");
+var quoteText = document.getElementById("quote-text");
+var quoteAuthor = document.getElementById("quote-author");
+var quoteFlex = document.getElementById("quote-flex");
+var tweetBtn = document.getElementById("tweet");
+
+addQuote(30);
+setQuote(quoteArr[0] = quoteArr1[Math.floor(Math.random()*50)]);
+
+nextBtn.addEventListener("click",nextButton);
+prevBtn.addEventListener("click",prevButton);
+tweetBtn.addEventListener("click",tweet);
+
+
+
+function nextButton() {
+  
+  if(quoteIndex===0){
+    prevBtn.classList.remove("disabled");
+  }
+  quoteIndex++;
+  if(quoteArr.length - quoteIndex < 10){
+    addQuote(30);
+  }
+  setQuote(quoteArr[quoteIndex]);
+  
+}
+
+function prevButton() {
+  quoteIndex--;
+  if(quoteIndex===0){
+    prevBtn.classList.add("disabled");
+  }
+  setQuote(quoteArr[quoteIndex]);
+}
+
+function setQuote(quoteObj){
+  var randomColor = Math.floor(Math.random()*colors.length);
+  $("#quote-flex, .btn-primary").css({
+    "background-color":colors[randomColor],
+    color:colors[randomColor]
+  });
+  $(".btn-primary").css("color","white");
+  
+  $(".quote").animate({
+          opacity: 0
+        }, 500,
+        function() {
+          $(this).animate({
+            opacity: 1,
+          }, 500);
+          $('#quote-text').html(quoteObj.quoteText);
+        });
+  $("#author").animate({
+          opacity: 0
+        }, 500,
+        function() {
+          $(this).animate({
+            opacity: 1
+          }, 500);
+          $("#quote-author").html(quoteObj.quoteAuthor);
+        });
+  tweet();
+}
+
+      
+
+function addQuote(num) {
+  num = num!=undefined?num:1;
+  for(i=0;i<num;i++){
+    var script = document.createElement("script");
+    script.src = "https://api.forismatic.com/api/1.0/?        method=getQuote&format=jsonp&key="+Math.floor(Math.random()*1000) +"&lang=en&jsonp=load";
+    document.body.appendChild(script);
+  };
+}
+
+function load(json) {
+  quoteArr.push(json);
+}
+
+function tweet(){
+  $("#tweet").attr("href","https://twitter.com/intent/tweet?hashtag=quotes&related=freecodecamp&text=" + encodeURIComponent('"'+quoteText.innerHTML+"'"+quoteAuthor.innerHTML));
+}
+  /*
+  document.getElementById("quote-text").innerHTML = json.quoteText;
+  document.getElementById("quote-author").innerHTML = json.quoteAuthor;
+  */
+  
+
+  
+/*
+function loadDoc() {
+  console.log("clicked");
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+      var response = JSON.parse(this.responseText);
+      document.getElementById("quote-text").innerHTML = response.quoteText;
+    }
+  };
+  xhttp.open("GET", "https://api.forismatic.com/api/1.0/method=getQuote&format=jsonp&lang=en",true);
+  xhttp.send();
+}
+*/
